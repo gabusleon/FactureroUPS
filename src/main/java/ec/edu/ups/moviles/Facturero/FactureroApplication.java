83d5ce3c8d2920a011c09aaaa0b429a81dc45910
Gabriel A. Leon Paredes <gabrielleonp@hotmail.com>
2023-01-04 15:03:48 -0500

    Se actualiza para el despliegue en la nube

--- src/main/java/ec/edu/ups/moviles/Facturero/FactureroApplication.java
+++ src/main/java/ec/edu/ups/moviles/Facturero/FactureroApplication.java
@@ -2,13 +2,19 @@ package ec.edu.ups.moviles.Facturero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class FactureroApplication {
+public class FactureroApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FactureroApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FactureroApplication.class);
+	}
 }
